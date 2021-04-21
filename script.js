35787647c6bd1ff8f5588ddcffbed7dc10229e95
julioasvn <59871300+julioasvn@users.noreply.github.com>
2021-04-21 09:50:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,404 @@
+var cartaUm = {
+    nome: "Bjorn",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-9/167584734_2843429242537070_2554099088812582867_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=730e14&_nc_ohc=DfMeqs4TO8sAX_qdtOB&_nc_ht=scontent.fgru5-1.fna&oh=6dba5bf98521f8a40170666513073d01&oe=608D3AC5",
+    atributos: {
+        ataque: 50,
+        defesa: 55,
+        magia: 5
+    }
+}
+var cartaDois = {
+    nome: "Arthurus",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-0/p526x296/167276543_2842690162610978_1130367722684277112_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=730e14&_nc_ohc=M9REfhoGgEwAX_ZhTXW&_nc_ht=scontent.fgru5-1.fna&tp=6&oh=2537119250a40e62aaa898d444ac70e7&oe=608A5498",
+    atributos: {
+        ataque: 25,
+        defesa: 25,
+        magia: 60
+    }
+}
+var cartaTres = {
+    nome: "Aresius",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-9/167457718_2843429225870405_7069013885154382669_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=730e14&_nc_ohc=m8aUIqAQSvUAX8xzmks&_nc_ht=scontent.fgru5-1.fna&oh=fd7dac518b624a47ee9a75e28d25a756&oe=608B2454",
+    atributos: {
+        ataque: 60,
+        defesa: 50,
+        magia: 5
+    }
+}
+var cartaQuatro = {
+    nome: "Felícia",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-9/168234537_2843429265870401_6025913824111269519_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=730e14&_nc_ohc=RDa7XCJ9bTUAX8lgZtX&_nc_ht=scontent.fgru5-1.fna&oh=3f1387a14c71b93506710b47b6d2ec56&oe=608B7A6E",
+    atributos: {
+        ataque: 50,
+        defesa: 60,
+        magia: 20
+    }
+}
+var cartaCinco = {
+    nome: "Kathya",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-9/168319983_2843429219203739_7564184422891258190_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=730e14&_nc_ohc=ci5ZqQXkQngAX9H1r-O&_nc_ht=scontent.fgru5-1.fna&oh=b9070655372c5031cd6259b80c0fb8cd&oe=608DA2C8",
+    atributos: {
+        ataque: 60,
+        defesa: 40,
+        magia: 25
+    }
+}
+var cartaSeis = {
+    nome: "Laiserinhgs",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-0/p526x296/167280286_2842692835944044_6252641053206758500_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=730e14&_nc_ohc=DCxU74c7ZsUAX-TqwGt&_nc_ht=scontent.fgru5-1.fna&tp=6&oh=036f743f9bf652373d6e9d183f35a61b&oe=608A8938",
+    atributos: {
+        ataque: 60,
+        defesa: 50,
+        magia: 20
+    }
+}
+var cartaSete = {
+    nome: "Lord Arvald",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-0/p526x296/167613440_2842693765943951_4109414820641840529_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=730e14&_nc_ohc=r_EOIjUOrrwAX9hZSUD&_nc_oc=AQnC5dgWXXnugZI2M9HFQmH83-IQlMCrMhE03P88fLc-wqDuDiKup1oPqBbw4UNQp8w&_nc_ht=scontent.fgru5-1.fna&tp=6&oh=d39344d3ee3959c10889477fff7c486c&oe=608AAD70",
+    atributos: {
+        ataque: 60,
+        defesa: 60,
+        magia: 5
+    }
+}
+var cartaOito = {
+    nome: "Orghanna",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-0/p526x296/167337014_2842694642610530_7012746077929866658_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=730e14&_nc_ohc=RhMv-54vYgAAX_DUvxU&_nc_ht=scontent.fgru5-1.fna&tp=6&oh=3deef223b7f682d6986d32a45844e697&oe=608C32B1",
+    atributos: {
+        ataque: 15,
+        defesa: 15,
+        magia: 60
+    }
+}
+var cartaNove = {
+    nome: "Samir",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-0/p526x296/167395964_2842695852610409_3820200044122243603_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=730e14&_nc_ohc=_XwHRKQxcsMAX-ne1Ch&_nc_ht=scontent.fgru5-1.fna&tp=6&oh=d258a7f52a5c8febab62af134c5665a3&oe=608C0F2F",
+    atributos: {
+        ataque: 60,
+        defesa: 40,
+        magia: 15
+    }
+}
+var cartaDez = {
+    nome: "Tamiris",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-0/p526x296/167455003_2842696575943670_7293984183379700532_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=730e14&_nc_ohc=wPcInZSuFL8AX8EaLt6&_nc_ht=scontent.fgru5-1.fna&tp=6&oh=6d5950969f4b113da93d81aa3db29d1d&oe=608A8E7D",
+    atributos: {
+        ataque: 10,
+        defesa: 15,
+        magia: 30
+    }
+}
+var cartaOnze = {
+    nome: "Thlorin",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-9/167848892_2843429235870404_1299661234839988238_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=730e14&_nc_ohc=A4PRvs64s_QAX8Ul99F&_nc_ht=scontent.fgru5-1.fna&oh=998261900cc6a7fed28ce4593d5622b1&oe=608BF0FC",
+    atributos: {
+        ataque: 60,
+        defesa: 60,
+        magia: 5
+    }
+}
+var cartaDoze = {
+    nome: "Wil",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-9/168845844_2843429412537053_5652242515010064713_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=730e14&_nc_ohc=EhVBngx2cy4AX_kRKQL&_nc_ht=scontent.fgru5-1.fna&oh=420870903b6e12e20992bc864c071e80&oe=608D70D1",
+    atributos: {
+        ataque: 20,
+        defesa: 50,
+        magia: 20
+    }
+}
+var cartaTreze = {
+    nome: "Zuri",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-0/p526x296/167482357_2842700099276651_4928800474367859307_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=730e14&_nc_ohc=gC_F3SBDg3sAX84hNjp&_nc_ht=scontent.fgru5-1.fna&tp=6&oh=e8a9a51d1355cf1116b7aebef6f8a1c8&oe=60888662",
+    atributos: {
+        ataque: 60,
+        defesa: 50,
+        magia: 5
+    }
+}
+var cartaQuatorze = {
+    nome: "Hegreus",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-0/p526x296/167457788_2842701805943147_3821056675923483711_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=730e14&_nc_ohc=qh_0RpxTnxwAX9qlL-K&_nc_ht=scontent.fgru5-1.fna&tp=6&oh=4d963cf24e65d6c212298aa3e04b4a0b&oe=608B56F9",
+    atributos: {
+        ataque: 15,
+        defesa: 15,
+        magia: 60
+    }
+}
+var cartaQuinze = {
+    nome: "Timeria",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-0/p526x296/167280286_2842703715942956_7488479356875833501_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=730e14&_nc_ohc=UGkPfGxIeJcAX_oisCx&_nc_ht=scontent.fgru5-1.fna&tp=6&oh=ba0f55560a9266956f7eb44eded5c677&oe=608B22CB",
+    atributos: {
+        ataque: 15,
+        defesa: 20,
+        magia: 65
+    }
+}
+var cartaDezesseis = {
+    nome: "Abbo",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-9/167604333_2843429222537072_4548652659761824557_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=730e14&_nc_ohc=VjSx8cn9o6QAX_PFDII&_nc_ht=scontent.fgru5-1.fna&oh=2d9242b4121640fd0a86bf05a4c2cae0&oe=608B1FFC",
+    atributos: {
+        ataque: 15,
+        defesa: 10,
+        magia: 10
+    }
+}
+var cartaDezessete = {
+    nome: "Leopoldo",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-0/p526x296/167397635_2842705455942782_6062344223786222387_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=730e14&_nc_ohc=aHxoqtwR1_YAX_Z9QWo&_nc_ht=scontent.fgru5-1.fna&tp=6&oh=9358ed7f465adb336ee0fbfab7b95003&oe=608AF108",
+    atributos: {
+        ataque: 80,
+        defesa: 70,
+        magia: 10
+    }
+}
+var cartaDezoito = {
+    nome: "Conde Yaroslav",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-0/p526x296/167417575_2842706319276029_7210777542435897442_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=730e14&_nc_ohc=VTzW7xT_72oAX94KTzy&_nc_ht=scontent.fgru5-1.fna&tp=6&oh=0cba0557c9f702e06107ccc649661c77&oe=608B101E",
+    atributos: {
+        ataque: 60,
+        defesa: 60,
+        magia: 50
+    }
+}
+var cartaDezenove = {
+    nome: "Anubis",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-0/p526x296/167389078_2842707949275866_1122997614766019955_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=730e14&_nc_ohc=VAxnelia2F8AX9a8SJZ&_nc_ht=scontent.fgru5-1.fna&tp=6&oh=bdd67cac780dbc42ca11a1355ed61765&oe=608A2E1E",
+    atributos: {
+        ataque: 60,
+        defesa: 50,
+        magia: 90
+    }
+}
+var cartaVinte = {
+    nome: "Indra",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-0/p526x296/167373969_2842708749275786_2645710431721785609_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=730e14&_nc_ohc=wXrI1tIVH2kAX9PWz5N&_nc_ht=scontent.fgru5-1.fna&tp=6&oh=51e4a1e0f0a3820355ec2490e264953d&oe=6089C565",
+    atributos: {
+        ataque: 70,
+        defesa: 50,
+        magia: 90
+    }
+}
+var cartaVinteEUm = {
+    nome: "Arcanjo Azhael",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-0/p526x296/167419279_2842712209275440_2266125782754733529_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=730e14&_nc_ohc=MpfZ61QHBFUAX9AvQNT&_nc_ht=scontent.fgru5-1.fna&tp=6&oh=479eab1710b917b91561e413411fc023&oe=6089C62A",
+    atributos: {
+        ataque: 90,
+        defesa: 50,
+        magia: 90
+    }
+}
+var cartaVinteEDois = {
+    nome: "THOR - SUPER TUNFO",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-9/168215226_2843429289203732_8867434930749688672_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=730e14&_nc_ohc=eV-L2ubE4IEAX8Rz-tQ&_nc_ht=scontent.fgru5-1.fna&oh=f88ae123781f7d987e6748772c3a4e0a&oe=608C7491",
+    atributos: {
+        ataque: 100,
+        defesa: 100,
+        magia: 100
+    }
+}
+var cartaVinteETres = {
+    nome: "Chang Kayne",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-9/167524500_2843429179203743_7519691021234657225_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=730e14&_nc_ohc=z0ADvghqtrAAX_rjrXj&_nc_ht=scontent.fgru5-1.fna&oh=dcbaebecae06ab7b302ae2e5e9bc8c45&oe=608C958D",
+    atributos: {
+        ataque: 60,
+        defesa: 50,
+        magia: 20
+    }
+}
+var cartaVinteEQuatro = {
+    nome: "Daniel",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-9/168042610_2843429202537074_1942800770495707686_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=730e14&_nc_ohc=-2DGPHpOXQAAX_PHh96&_nc_oc=AQnoD2xCyv0-9grWSXRwTUwsdTsXGULihKQWiPG4gx7GngE3iMlumNWXb62hhsQBty8&_nc_ht=scontent.fgru5-1.fna&oh=78810fc7da5449d46296cd52eec1a132&oe=608C5056",
+    atributos: {
+        ataque: 60,
+        defesa: 20,
+        magia: 10
+    }
+}
+var cartaVinteECinco = {
+    nome: "Ivan",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-9/167838944_2843429329203728_3782426174194115696_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=730e14&_nc_ohc=l6nLZHXKvwIAX-ZCsI3&_nc_ht=scontent.fgru5-1.fna&oh=7e4e080fd689b389c311716818aff760&oe=608D86A0",
+    atributos: {
+        ataque: 55,
+        defesa: 20,
+        magia: 20
+    }
+}
+var cartaVinteESeis = {
+    nome: "Jack",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-9/167056899_2843429342537060_1490596192979944922_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=730e14&_nc_ohc=XjjwgwJEob4AX_Gc6FD&_nc_ht=scontent.fgru5-1.fna&oh=75e0ed6f7b2258f6a17169eeb366406d&oe=608B14EC",
+    atributos: {
+        ataque: 40,
+        defesa: 20,
+        magia: 30
+    }
+}
+var cartaVinteESete = {
+    nome: "Logan",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-9/167961288_2843429352537059_5660502708769085819_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=730e14&_nc_ohc=5DegrsqCFTkAX_se5lu&_nc_ht=scontent.fgru5-1.fna&oh=82320f363056f085b550262b2d8f7a47&oe=608D99A0",
+    atributos: {
+        ataque: 90,
+        defesa: 90,
+        magia: 10
+    }
+}
+var cartaVinteEOito = {
+    nome: "Sargento",
+    imagem: "https://scontent.fgru5-1.fna.fbcdn.net/v/t1.6435-9/168045232_2843429405870387_4694868935166746057_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=730e14&_nc_ohc=JRvvOeoqyjEAX9mn8Hi&_nc_ht=scontent.fgru5-1.fna&oh=7b1e7a79f9fe911f6834ffef01a6810a&oe=608A6395",
+    atributos: {
+        ataque: 95,
+        defesa: 95,
+        magia: 10
+    }
+}
+
+var cartaMaquina
+var cartaJogador
+var cartas = [cartaUm, cartaDois, cartaTres, cartaQuatro, cartaCinco, cartaSeis, cartaSete, cartaOito, cartaNove, cartaDez, cartaOnze, cartaDoze, cartaTreze, cartaQuatorze, cartaQuinze, cartaDezesseis, cartaDezessete, cartaDezoito, cartaDezenove, cartaVinte, cartaVinteEUm, cartaVinteEDois, cartaVinteETres, cartaVinteEQuatro, cartaVinteECinco, cartaVinteESeis, cartaVinteESete,  cartaVinteEOito]
+
+var pontosJogador = 0
+var pontosMaquina = 0
+
+atualizaPlacar()
+atualizaQuantidadeDeCartas()
+
+function atualizaQuantidadeDeCartas(){
+  var divQuantidadeCartas = document.getElementById('quantidade-cartas')
+  var html = "Cartas no baralho: " + cartas.length
+  
+  divQuantidadeCartas.innerHTML = html
+}
+
+function atualizaPlacar(){
+  var divPlacar = document.getElementById('placar')
+  var html = "Jogador " + pontosJogador + " X " + pontosMaquina + " Máquina"
+  divPlacar.innerHTML = html
+}
+
+function sortearCarta() {
+    var numeroCartaMaquina = parseInt(Math.random() * cartas.length)
+    cartaMaquina = cartas[numeroCartaMaquina]
+    cartas.splice(numeroCartaMaquina, 1)
+  
+    //var numeroCartaJogador = 26
+    var numeroCartaJogador = parseInt(Math.random() * cartas.length)
+    /*while (numeroCartaJogador == numeroCartaMaquina) {
+        numeroCartaJogador = parseInt(Math.random() * 22)
+    }*/
+    cartaJogador = cartas[numeroCartaJogador]
+    cartas.splice(numeroCartaJogador, 1)
+    //console.log(cartaJogador)
+
+    document.getElementById('btnSortear').disabled = true
+    document.getElementById('btnJogar').disabled = false
+  
+    exibeCartaJogador()
+    //exibirOpcoes()
+}
+
+function exibeCartaJogador(){
+  var divCartaJogador = document.getElementById("carta-jogador")
+  var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">'  
+  divCartaJogador.style.backgroundImage=`url(${cartaJogador.imagem})`
+  var nome = `<p class='carta-subtitle'>${cartaJogador.nome}</p>`  
+  var opcoesTexto = ""
+    
+  for (var atributo in cartaJogador.atributos) {
+        opcoesTexto += "<input type='radio' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaJogador.atributos[atributo] + "<br>"
+  }
+  
+  //opcoes.innerHTML = opcoesTexto
+  
+  var html = "<div id='opcoes' class='carta-status'>"
+  
+  divCartaJogador.innerHTML = moldura + nome + html + opcoesTexto + "</div>"
+}
+
+/*function exibirOpcoes() {
+    var opcoes = document.getElementById('opcoes')
+    var opcoesTexto = ""
+    for (var atributo in cartaJogador.atributos) {
+        opcoesTexto += "<input type='radio' name='atributo' value='" + atributo + "'>" 
+    }
+    opcoes.innerHTML = opcoesTexto
+}
+*/
+
+function obtemAtributoSelecionado() {
+    var radioAtributo = document.getElementsByName('atributo')
+    for (var i = 0; i < radioAtributo.length; i++) {
+        if (radioAtributo[i].checked) {
+            return radioAtributo[i].value
+        }
+    }
+}
+
+function jogar() {
+    var divResultado = document.getElementById("resultado")
+    var atributoSelecionado = obtemAtributoSelecionado()
+    
+    exibeCartaMaquina()
+
+    if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
+        //alert('Venceu a carta máquina')
+        htmlResultado = '<p class="resultado-final">VENCEU!</p>'
+        pontosJogador++
+    } else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
+        //alert('Perdeu. Carta da máquina é maior')
+        htmlResultado = '<p class="resultado-final">PERDEU!</p>'
+        pontosMaquina++
+    } else {
+        //alert('Empatou!')
+      htmlResultado = '<p class="resultado-final">EMPATOU!</p>'
+    }
+  
+  if(cartas.length == 0){
+    alert("Fim de Jogo!")
+    if(pontosJogador > pontosMaquina){
+      htmlResultado = '<p class="resultado-final">O HUMANO VENCEU!</p>'
+    }else if(pontosJogador < pontosMaquina){
+      htmlResultado = '<p class="resultado-final">A MÁQUINA VENCEU!</p>'
+    }else{
+      htmlResultado = '<p class="resultado-final">A PARTIDA EMPATOU!</p>'
+    }
+  }else{
+     document.getElementById('btnProximaRodada').disabled = false  
+  }
+  
+  divResultado.innerHTML = htmlResultado 
+  document.getElementById('btnJogar').disabled = true
+  
+  atualizaPlacar()
+  atualizaQuantidadeDeCartas()
+  
+}
+
+function exibeCartaMaquina(){
+  
+  var divCartaMaquina = document.getElementById("carta-maquina")
+  var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">'
+      
+  divCartaMaquina.style.backgroundImage=`url(${cartaMaquina.imagem})`
+  var nome = `<p class='carta-subtitle'>${cartaMaquina.nome}</p>`
+  
+  var opcoesTexto = ""
+    
+  for (var atributo in cartaMaquina.atributos) {
+     opcoesTexto += "<p type='txt' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaMaquina.atributos[atributo] + "<br>"
+  }
+  
+  //opcoes.innerHTML = opcoesTexto
+  
+  var html = "<div id='opcoes' class='carta-status'>"
+  
+  divCartaMaquina.innerHTML = moldura + nome + html + opcoesTexto + "</div>"
+} 
+
+function proximaRodada(){
+  var divCartas = document.getElementById('cartas')
+  divCartas.innerHTML = `<div id = "carta-jogador" class = "carta"></div> <div id = "carta-maquina" class = "carta"></div>`
+  document.getElementById('btnSortear').disabled = false
+  document.getElementById('btnJogar').disabled = true
+  document.getElementById('btnProximaRodada').disabled = true
+  var divResultado = document.getElementById('resultado')
+  divResultado.innerHTML = ""
+}
